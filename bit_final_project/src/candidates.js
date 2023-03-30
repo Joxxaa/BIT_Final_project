9963e16aabf44063a8d8c980e3813e43c2ba42ae
sasajovkovic@gmail.com <sasajovkovic@gmail.com>
2023-03-30 12:41:41 +0200

    Searh bar - ne funkcioniše Link ka podacima kandidata

--- bit_final_project/src/candidates.js
+++ bit_final_project/src/candidates.js
@@ -1,8 +1,12 @@
 import './App.css';
+import './reports';
 import React, { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const UsingFetchCand = () => {
     const [candidates, setCandidates] = useState([])
+    const [searchInput, setSearchInput] = useState("");
 
     const fetchData = () => {
         fetch("http://localhost:3333/api/candidates")
@@ -18,23 +22,80 @@ const UsingFetchCand = () => {
         fetchData()
     }, [])
 
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+
+    if (searchInput.length > 0) {
+        candidates.filter((candidate) => {
+            return candidate.name.match(searchInput);
+        });
+    }
+
+
     return (
-        <div id="cards">
-            {candidates.length > 0 && (
+        <div>
+            <div id='search'>
+                <span>
+                    Candidates
+                </span>
+                <input
+                    type="text"
+                    placeholder="Search here"
+                    onChange={handleChange}
+                    value={searchInput}
+                />
+            </div>
+            {candidates.map((candidate) => {
+
                 <div>
-                    {candidates.map(candidate => (
-                        <div id="kard" key={candidate.id} >
-                            <div class="card"  >
-                                <img src={candidate.avatar} class="card-img-top" alt="..." width={200}/>
-                                <div class="card-body">
-                                    <h5 class="card-title">{candidate.name}</h5>
-                                    <p class="card-text">{candidate.email.toLowerCase()}</p>
+                    
+                        <p>{candidate.name}</p>
+                        <p>{candidate.email}</p>
+                    
+                </div>
+
+            })}
+            <hr></hr>
+            <div id="cards">
+                {candidates.length > 0 && (
+                    <div>
+                        {candidates.map(candidate => (
+
+                            <div className="col-lg-3 col-md-4 col-sm-6">
+                                <div className="card">
+                                    <div className="round-img">
+                                        <Link to={`/reports/${candidate.id}`}>
+                                            <img // on click on picture for getting id and sending via props.match... to component CandidateReport
+                                                className="card-img-top"
+                                                src={candidate.avatar}
+                                                alt="Card cap"
+                                                width={200}
+                                            />
+                                        </Link>
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{candidate.name}</h5>
+                                        <p className="card-text">{candidate.email}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            )}
+                            // <Card key={candidate.id} border="secondary" style={{ width: '18rem' }}>
+                            //     <Card.Img variant="top" src={candidate.avatar} width={200}/>
+                            //     <Card.Body>
+                            //         <Card.Title>{candidate.name}</Card.Title>
+                            //         <Card.Text>
+                            //         {candidate.email.toLowerCase()}
+                            //         </Card.Text>
+                            //     </Card.Body>
+                            // </Card>
+
+
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
--- bit_final_project/src/reports.js
+++ bit_final_project/src/reports.js
@@ -1,8 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from "react"
 
-const UsingFetchRep = () => {
+const UsingFetchRep = (props) => {
     const [reports, setReports] = useState([])
+    const [candidateInfo, setCandidateInfo] = useState([]);
+    let idCandidate = props.match.params.id;
+    
   
     const fetchData = () => {
       fetch("http://localhost:3333/api/reports")
@@ -17,23 +20,48 @@ const UsingFetchRep = () => {
     useEffect(() => {
       fetchData()
     }, [])
+
+    useEffect(() => {
+        // getting single candidate via id
+        fetch(`http://localhost:3333/api/candidates/${idCandidate }`)
+        .then((result) => {
+          return result.json();
+        }).then((item) => {
+          setCandidateInfo(item);
+        });
+       
+      }, []);
+
+    
   
+    
+
+    let singleReport = reports.filter((report) => {
+        if (parseInt(idCandidate) === report.candidateId) {
+          return report;
+        }
+      });
+
     return (
-      <div>
-        {reports.length > 0 && (
-          <ul>
-            {reports.map(report => (
-              <li key={report.id}>
-                {report.candidateName},
-                {report.companyName},
-                {report.status},
-                {report.note}
-                </li>
-            ))}
-          </ul>
-        )}
-      </div>
+        <div>
+        singleReport={singleReport}
+        </div>
+    //   <div>
+    //     {reports.length > 0 && (
+    //       <ul>
+    //         {reports.map(report => (
+    //           <li key={report.id}>
+    //             {report.candidateName},
+    //             {report.companyName},
+    //             {report.status},
+    //             {report.note}
+    //             </li>
+    //         ))}
+    //       </ul>
+    //     )}
+    //   </div>
+    
     )
   }
 
-  export default UsingFetchRep;
+  export default UsingFetchRep ;
